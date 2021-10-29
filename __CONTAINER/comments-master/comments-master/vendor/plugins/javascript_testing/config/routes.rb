fb0c32c82933647848490d535e6850e81fc65f12
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 06:57:39 +0000

    -update

--- __CONTAINER/comments-master/comments-master/vendor/plugins/javascript_testing/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do |map|
-  if Rails.env != 'production'
-    get "javascript_testing/index" => "javascript_testing#index"
-  end
-end
--- __CONTAINER/comments-master/comments-master/vendor/plugins/javascript_testing/lib/javascript_testing.rb
+++
@@ -1,5 +0,0 @@
-module JavascriptTesting
-  class Engine < Rails::Engine
-    engine_name :javascript_testing
-  end
-end
